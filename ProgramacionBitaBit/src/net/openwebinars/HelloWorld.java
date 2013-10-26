b57a80139c9dc90c3c9f9ddb3fbe57490ef77e69
CrmPrograming <crmprograming@gmail.com>
2013-10-26 11:47:13 +0100

    Commit inicial para HelloWorld

---
+++ ProgramacionBitaBit/src/net/openwebinars/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Clase HelloWorld
+ * 
+ * Programa a modo de ilustración
+ * del típico programa HelloWorld
+ */
+
+package net.openwebinars;
+
+public class HelloWorld {
+  
+  /* Main principal
+   * para el programa "HelloWorld"
+   */
+  public static void main(String[] args) {
+    
+    // Imprimimos por consola el mensaje
+    System.out.println("Hello World, hello OpenWebinars.net!");
+  }
+
+}
